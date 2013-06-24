43c26290845641c849c2c885855565ca1b79c22c
Steffan Harries <steffanharries@googlemail.com>
2013-06-24 21:15:13 +0100

    Fix never 404ing

--- index.php
+++ index.php
@@ -2,23 +2,54 @@
 require_once __DIR__.'/vendor/autoload.php';
 
 use Symfony\Component\Translation\Loader\YamlFileLoader;
+use Symfony\Component\HttpKernel\Debug\ExceptionHandler;
+
+// Handle exceptions nicely
+ExceptionHandler::register(false);
 
 $app = new Silex\Application();
-$app['debug'] = true;
+//$app['debug'] = true;
 $app->register(new Silex\Provider\UrlGeneratorServiceProvider());
+$app->register(new Silex\Provider\TranslationServiceProvider(), array());
+$app->register(new Silex\Provider\SessionServiceProvider());
+
+$app['session']->start();
+
+// Setup translations
+$app['app_default_locale'] = 'en';
+$app['app_allowed_locales'] = array('en','cy');
+
+$locale = function() use ($app) {
+    $path = $_SERVER['REQUEST_URI'];
+
+    $request_tokens = explode('/', $path);
+    $locale = in_array($request_tokens[1], $app['app_allowed_locales']) ? $request_tokens[1] : null;
+    $localeFromSession = $app['session']->get('locale');
+
+    if (null == $locale) {
+        $app->abort(404, 'Sorry, the page you are looking for could not be found.');
+    }
+
+    if($locale != $localeFromSession) {
+        $app['session']->set('locale', $locale);
+    }
+
+    return $locale;
+};
+
 $app->register(new Silex\Provider\TranslationServiceProvider(), array(
-    'locale_fallback' => 'en',
+    'locale' => $locale,
+    'locale_fallback' => $app['app_default_locale'],
 ));
 
-$app['translator'] = $app->share($app->extend('translator', function($translator, $app) {
+$app['translator'] = $app->share($app->extend('translator', function ($translator, $app) {
     $translator->addLoader('yaml', new YamlFileLoader());
-
-    $translator->addResource('yaml', __DIR__.'/translations/en.yml', 'en');
-    $translator->addResource('yaml', __DIR__.'/translations/cy.yml', 'cy');
-
+    $translator->addResource('yaml', __DIR__ . '/translations/en.yml', 'en');
+    $translator->addResource('yaml', __DIR__ . '/translations/cy.yml', 'cy');
     return $translator;
 }));
 
+// Routes
 $app->register(new Silex\Provider\TwigServiceProvider(), array(
   'twig.path' => __DIR__.'/views',
 ));
@@ -28,7 +59,7 @@ $app->get('/', function() use($app) {
 });
 
 $app->get('/{_locale}/', function() use($app) {
-  return $app['twig']->render('index.html.twig', array ());
+    return $app['twig']->render('index.html.twig', array ());
 });
 
 $app->get('/{_locale}/services/', function() use($app) {
